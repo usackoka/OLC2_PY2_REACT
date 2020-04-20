22bee0d6a2428b968e6b010bc586fc6e1c6dae9c
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-04-20 17:35:46 -0600

    add gulp and ts

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+// Task para compilar TypeScript
+gulp.task('typescript', () => {
+    // Seleccionar todos los archivos .ts
+    var tsResult = gulp.src('./src/interprete/AST/*.ts')
+    .pipe(ts({
+    noImplicitAny: true
+ }));
+    // Destino del proyecto compilado
+    return tsResult.js.pipe(gulp.dest('./dist/interprete/AST'));
+});
+
+// Task para observar los cambios al proyecto
+gulp.task('watch', () => {
+    // Observar proyecto
+    gulp.watch('./*.ts', gulp.series('typescript'));
+});
+
+// Trigger a los tasks
+gulp.task('default', gulp.parallel('typescript', 'watch'));

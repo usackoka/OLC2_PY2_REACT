760081a9da99e28399c51aff4d43195b5ed63242
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-05-02 21:28:01 -0600

    Clase Aritmetica, falta probar

--- src/compilador/AST/Expresiones/Aritmetica.ts
+++ src/compilador/AST/Expresiones/Aritmetica.ts
@@ -1,5 +1,6 @@
 import { Entorno } from "../Entorno";
 import { Expresion } from "../Expresion";
+import { Casteo } from "./Casteo";
 
 export class Aritmetica extends Expresion{
 
@@ -17,7 +18,23 @@ export class Aritmetica extends Expresion{
     }
 
     public getTraduccion(entorno:Entorno):string{
-        return "";
+        switch(this.TIPO_OPERACION){
+            case Aritmetica.TYPE.SUMA:
+                return this.SUMA(entorno);
+            case Aritmetica.TYPE.RESTA:
+                return this.RESTA(entorno);
+            case Aritmetica.TYPE.MULTIPLICACION:
+                return this.MULTIPLICACION(entorno);
+            case Aritmetica.TYPE.DIVISION:
+                return this.DIVISION(entorno);
+            case Aritmetica.TYPE.MODULAR:
+                return this.MODULAR(entorno);
+            case Aritmetica.TYPE.POTENCIA:
+                return this.POTENCIA(entorno);
+            default:
+                entorno.addError("ARITMETICA-TRAD:"+this.TIPO_OPERACION,"No se encontró este tipo de Operación",this.fila,this.columna);
+                return "0";
+        }
     }
 
     public getTipo(entorno:Entorno):Object{
@@ -69,6 +86,164 @@ export class Aritmetica extends Expresion{
                 return Expresion.State.INTEGER;
             }
         }
+    }
+
+    public SUMA(entorno:Entorno):string{
+        let izq:string = this.izquierda.getTraduccion(entorno).toString();
+        let der:string = this.derecha.getTraduccion(entorno).toString();
+        let tipIzq:Object = this.izquierda.getTipo(entorno);
+        let tipDer:Object = this.derecha.getTipo(entorno);
+
+        if (tipIzq == Expresion.State.STRING || tipDer == Expresion.State.STRING)
+        {
+            if (tipIzq == Expresion.State.STRING  && tipDer != Expresion.State.STRING)
+            {
+                let n:Casteo = new Casteo(Expresion.State.STRING, this.derecha, this.fila, this.columna);
+                der = n.getTraduccion(entorno).toString();
+            }
+            else if (tipDer == Expresion.State.STRING && tipIzq != Expresion.State.STRING)
+            {
+                let n:Casteo = new Casteo(Expresion.State.STRING, this.izquierda, this.fila, this.columna);
+                izq = n.getTraduccion(entorno).toString();
+            }
+
+            //si ambos son strings, suma de strings
+            let t1 = entorno.getTemp();
+            let t2 = entorno.getTemp();
+            let t3 = entorno.getTemp();
+            let t4 = entorno.getTemp();
+            let tretorno = entorno.getTemp();
+            let l1 = entorno.getETQ();
+            let l2 = entorno.getETQ();
+            let l3 = entorno.getETQ();
+
+            entorno.addComentario("=============== Suma de strings ===================");
+            entorno.addValor(t1, izq);
+            entorno.addValor(t2, der);
+            entorno.addValor(tretorno, "H");
+            entorno.addComentario("====== copio primer string");
+            entorno.addETQ(l2);
+            entorno.addGetHeap(t3, t1);
+            entorno.addIgualQue(t3, "3", l1);
+            entorno.addValorEnHeap("H", t3);
+            entorno.incH();
+            entorno.addValorOperacion(t1, t1, "+", "1");
+            entorno.addGoto(l2);
+            entorno.addComentario("====== copio segundo string");
+            entorno.addETQ(l1);
+            entorno.addGetHeap(t4, t2);
+            entorno.addIgualQue(t4, "3", l3);
+            entorno.addValorEnHeap("H", t4);
+            entorno.incH();
+            entorno.addValorOperacion(t2, t2, "+", "1");
+            entorno.addGoto(l1);
+            
+            entorno.addETQ(l3);
+            entorno.addValorEnHeap("H", "3");
+            entorno.incH();
+            entorno.addComentario("=================fin suma de strings====================");
+
+            //guardo los usados
+            entorno.addTempUsed(t1);
+            entorno.addTempUsed(t2);
+            entorno.addTempUsed(t3);
+            entorno.addTempUsed(t4);
+            //guardo los ya usados
+            entorno.addTempUsed(izq);
+            entorno.addTempUsed(der);
+            return tretorno;
+        }
+        else {
+            let retorno:string = entorno.getTemp();
+            entorno.addValorOperacion(retorno, izq, "+", der);
+
+            //guardo los ya usados
+            entorno.addTempUsed(izq);
+            entorno.addTempUsed(der);
+            return retorno;
+        }
+    }
+
+    public RESTA(entorno:Entorno):string{
+        let izq:string = this.izquierda.getTraduccion(entorno).toString();
+        let der:string = this.derecha.getTraduccion(entorno).toString();
+        let retorno:string = entorno.getTemp();
+        
+        entorno.addValorOperacion(retorno, izq, "-", der);
+        
+        entorno.addTempUsed(izq);
+        entorno.addTempUsed(der);
+        return retorno;
+    }
+
+    public MODULAR(entorno:Entorno):string{
+        let izq:string = this.izquierda.getTraduccion(entorno).toString();
+        let der:string = this.derecha.getTraduccion(entorno).toString();
+        let retorno:string = entorno.getTemp();
+        
+        entorno.addValorOperacion(retorno,izq,"%",der);
+        //guardo los ya usados
+        entorno.addTempUsed(izq);
+        entorno.addTempUsed(der);
+        return retorno;
+    }
+
+    public DIVISION(entorno:Entorno):string{
+        let izq:string = this.izquierda.getTraduccion(entorno).toString();
+        let der:string = this.derecha.getTraduccion(entorno).toString();
+        let retorno:string = entorno.getTemp();
+        entorno.addValorOperacion(retorno,izq,"/",der);
+        //guardo los ya usados
+        entorno.addTempUsed(izq);
+        entorno.addTempUsed(der);
+        return retorno;
+    }
+
+    public MULTIPLICACION(entorno:Entorno):string{
+        let izq:string = this.izquierda.getTraduccion(entorno).toString();
+        let der:string = this.derecha.getTraduccion(entorno).toString();
+        let retorno:string = entorno.getTemp();
+        entorno.addValorOperacion(retorno,izq,"*",der);
+        //guardo los ya usados
+        entorno.addTempUsed(izq);
+        entorno.addTempUsed(der);
+        return retorno;
+    }
+
+    public POTENCIA(entorno:Entorno):string{
+        entorno.addComentario("============ Llamada Potencia =====================");
+        let izq:string = this.izquierda.getTraduccion(entorno).toString();
+        let der:string = this.derecha.getTraduccion(entorno).toString();
+        let retorno:string = entorno.getTemp();
+        let t22 = entorno.getTemp();
+        let t23 = entorno.getTemp();
+        let t24 = entorno.getTemp();
+        
+        entorno.addComentario("//cambio simulado de ambito");
+        entorno.addValorOperacion(t22, "P","+", entorno.size);
+        entorno.addComentario("//posicion parametro 1");
+        entorno.addValorOperacion(t23, t22, "+", 1);
+        entorno.addComentario("//valor base");
+        entorno.addValorEnStack(t23, izq);
+        entorno.addComentario("//posicion paremtro 2");
+        entorno.addValorOperacion(t24, t22, "+", 2);
+        entorno.addComentario("//valor exponente");
+        entorno.addValorEnStack(t24, der);
+        entorno.addValorOperacion("P","P", "+", entorno.size);
+        entorno.addCall("funcion_nativa_potencia");
+        entorno.addGetStack(retorno, "P");
+        entorno.addValorOperacion("P","P", "-", entorno.size);
+        
+        //guardo los ya usados
+        entorno.addTempUsed(izq);
+        entorno.addTempUsed(der);
+        entorno.addTempUsed(t22);
+        entorno.addTempUsed(t23);
+        entorno.addTempUsed(t24);
+        
+        entorno.addComentario("============ FIN LLAMADA Potencia =====================");
+
+        return retorno;
     }
 }
 
